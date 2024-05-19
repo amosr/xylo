d84ea8e4128e54f9a486cb01480e210196962b12
Amos Robinson <amos.robinson@gmail.com>
2024-05-19 16:16:13 +1000

    experiments

--- py/xylo/cut.py
+++ py/xylo/cut.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+import jax_cosmo.scipy.interpolate as interpolate
 
 from xylo.types import CutCubic, BarProps, Sections
 
@@ -16,3 +17,39 @@ def cubic(bar: BarProps, cut: CutCubic) -> Sections:
     xmids = jnp.append( - jnp.flip(x), x)
     depths = jnp.append( jnp.flip(yclamp), yclamp)
     return Sections(xmids = xmids, depths = depths, length_per_element = l)
+
+def quartic(bar: BarProps, cut: jnp.ndarray) -> Sections:
+    # length of each section
+    l = bar.length / bar.elements
+    # midpoint of each section
+    x = jnp.linspace(0 + l/2, bar.length / 2 - l/2, bar.elements // 2)
+    # cut-out
+    y = cut[3] * (x ** 4) + cut[2] * (x ** 3) + cut[1] * (x ** 2) + cut[0]
+    # clamped
+    yclamp = jnp.maximum(jnp.minimum(y, bar.depth), bar.min_depth)
+    
+    xmids = jnp.append( - jnp.flip(x), x)
+    depths = jnp.append( jnp.flip(yclamp), yclamp)
+    return Sections(xmids = xmids, depths = depths, length_per_element = l)
+
+def spline(bar: BarProps, cut: jnp.ndarray, max_spread: float = 0.8, symmetric: bool = True) -> Sections:
+    cut_depths, spread = cut[0:-1], cut[-1]
+    if symmetric:
+        cut_depths = jnp.append(cut_depths, jnp.flip(cut_depths))
+    cut_depths = jnp.concatenate([jnp.array([1.0]), cut_depths, jnp.array([1.0])])
+    # length of each section
+    l = bar.length / bar.elements
+    bl2 = bar.length/2
+    cutx = bl2 * spread * max_spread
+    x = jnp.linspace(-bl2 + l/2, bl2 - l/2, bar.elements)
+
+    xi = jnp.linspace(-cutx, cutx, len(cut_depths))
+
+    yi = bar.min_depth + cut_depths * (bar.depth - bar.min_depth)
+
+    i = interpolate.InterpolatedUnivariateSpline(xi, yi, k=2)
+    y = jnp.where(jnp.logical_and(x <= cutx, x >= -cutx), i(x), bar.depth)
+
+    yclamp = jnp.maximum(jnp.minimum(y, bar.depth), bar.min_depth)
+    
+    return Sections(x, yclamp, length_per_element = l)
--- py/xylo/loss/receptance.py
+++ py/xylo/loss/receptance.py
@@ -7,13 +7,13 @@ import xylo.types as t
 import xylo.receptance as r
 import xylo.cut
 
-def loss(cut: t.CutCubic, wood: t.Wood, bar: t.BarProps, ws: jnp.ndarray):
+def loss(cut: t.CutCubic, wood: t.Wood, bar: t.BarProps, fundamental: float, ws: jnp.ndarray):
     sections = xylo.cut.cubic(bar, cut)
     wsT = ws.T
-    rs  = jax.vmap(lambda w: r.receptance_scalar(wood, bar, sections, w))(wsT[0] * 2 * jnp.pi)
-    jax.debug.print("{rs}", rs=rs)
+    rs  = jax.vmap(lambda w: r.receptance_scalar(wood, bar, sections, w))(wsT[0] * 2 * jnp.pi * fundamental)
+    # jax.debug.print("{rs}", rs=rs)
     return -jnp.dot(rs, wsT[1])
 
 def loss_(cut: t.CutCubic, args):
-  wood, bar, ws = args
-  return loss(cut, wood, bar, ws)
+  wood, bar, fundamental, ws = args
+  return loss(cut, wood, bar, fundamental, ws)
