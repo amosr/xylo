9bb09e93ea97bdcfc9db72f8552eb0447ba48e03
Amos Robinson <amos.robinson@gmail.com>
2024-06-23 02:02:41 +0000

    update weights

--- py/optimize_bars.py
+++ py/optimize_bars.py
@@ -19,28 +19,18 @@ import xylo.database
 
 tuning = xylo.tuning.yamaha_YX500R
 
-for num_dims in range(3,10):
-  options = xylo.opt.Options(num_generations = 10, strategy = evosax.OpenES(popsize = 3000, num_dims = num_dims), absolute_tolerance = 1e-2)
-  init_mean = None
-
-  for note in range(86, 56, -1):
-  # for note in range(57, 89):
-  # for note in range(70, 89):
-  # for note in [87]:
+for note in range(86, 56, -1):
+  for num_dims in range(3,10):
+    options = xylo.opt.Options(num_generations = 10, strategy = evosax.OpenES(popsize = 3000, num_dims = num_dims), absolute_tolerance =  1e-2)
     db = xylo.database.get()
     best = db.get_best_for_dims(note, num_dims)
-    if best is not None:
-      init_mean = best
-
-    options_ = options._replace(init_mean = init_mean)
-    # options_ = options
+    options_ = options._replace(init_mean = best)
 
     bar = db.get_bar(note)
     wood = db.get_wood(note)
 
     fundamental = tuning.note_to_freq(note)
     partials, weights = tuning.note_to_weights(note)
-    print(f"weights {weights}")
     sol = xylo.opt.optimize_geometry(bar, wood, options_, fundamental, jnp.array(partials), jnp.array(weights))
     spline = sol.best_member
 
@@ -52,6 +42,6 @@ for num_dims in range(3,10):
     swp = xylo.sweep.sweep(wood, bar, sections, t.sweep_default)
 
     print(note)
-    print("---------------")
+    print(f"--------------- (loss {sol.best_fitness})")
     print(swp.harmonics / fundamental)
     print("")
